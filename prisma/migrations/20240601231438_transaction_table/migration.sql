8165c64c041b67c26194503796a6dc40a264248e
steniomoreira <steniomoreiradev@gmail.com>
2024-06-01 20:39:12 -0300

    feat: project setup with fastify and prisma

---
+++ prisma/migrations/20240601231438_transaction_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "transaction" (
+    "id" TEXT NOT NULL,
+    "isPaid" BOOLEAN NOT NULL,
+    "dueDate" DATE NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "transaction_pkey" PRIMARY KEY ("id")
+);
