7ddf8be399534e9cc16d08c44c6e15482d70d9d0
steniomoreira <steniomoreiradev@gmail.com>
2024-06-01 23:01:09 -0300

    feat: add route and validations

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,9 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from "zod"
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify()
 
-const prisma = new PrismaClient()
-
-app.post('/transaction', async (request, reply) => {
-  const createTransaction = z.object({
-    isPaid: z.boolean(),
-    dueDate: z.string().transform((str) => new Date(str)),
-    description: z.string(),
-    amount: z.number(),
-    type: z.enum(['credit', 'debit'])
-  })
-
-  const {isPaid, dueDate, description, amount, type} = createTransaction.parse(request.body)
-
-  await prisma.transaction.create({
-    data: {isPaid, dueDate, description, amount: type === 'credit' ? amount : amount * -1 }
-  })  
-  
-  return reply.status(201).send()
-})
+app.register(transactionsRoutes)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running!');  
